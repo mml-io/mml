0feac7b1559c9e7b7ca558a7f1a226517f2a059f
Ben Miller <benito.m@gmail.com>
2023-06-13 14:37:42 +0100

    Contribution by Ben Miller

--- check-node-version.js
+++ check-node-version.js
@@ -1,21 +1,21 @@
 const { readFileSync } = require("fs");
 const { resolve } = require("path");
 
+let nvmrc;
+try {
+  nvmrc = readFileSync(resolve(process.cwd(), ".nvmrc"), { encoding: "utf8" });
+} catch (e) {
+  console.error("No .nvmrc file present");
+  process.exit(1);
+}
 
+const currentVersion = process.version.trim();
+const nvmrcVersion = nvmrc.trim();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (currentVersion === nvmrcVersion) {
+  console.log(`Node version matches: ${currentVersion}`);
+} else {
+  console.error(`Node version mismatch (${currentVersion} != ${nvmrcVersion}).`);
+  console.log(`Run 'nvm install ${nvmrcVersion}' to resolve.`);
+  process.exit(1);
+}
--- packages/schema-validator/src/validate.ts
+++ packages/schema-validator/src/validate.ts
@@ -1,11 +1,11 @@
 import { schemaXML } from "@mml-io/mml-schema";
-
+import validateAgainstSchema from "xsd-validator";
 
 export function validateMMLDocument(document: string): Array<Error> | null {
-
-
-
-
-
-
+  const result = validateAgainstSchema(document, schemaXML);
+  if (result === true) {
+    return null;
+  } else {
+    return result;
+  }
 }
