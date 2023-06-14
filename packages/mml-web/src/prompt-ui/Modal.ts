4f7b401b73b2b3af2a5417f9ca151b88dd7ffecd
Marcus Longmuir <marcus@mpl.dev>
2023-06-14 21:47:52 +0100

    Set prompt modal font color to black

--- packages/mml-web/src/prompt-ui/Modal.ts
+++ packages/mml-web/src/prompt-ui/Modal.ts
@@ -8,6 +8,7 @@ export class Modal {
     this.element.style.display = "block";
     this.element.style.border = "1px solid #AAA";
     this.element.style.fontFamily = "sans-serif";
+    this.element.style.color = "black";
 
     this.element.style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.1)";
     this.element.style.backdropFilter = "blur(4px)";
