3e7f97d724efcf078e80992ede1c9b747fcee745
Lukas Ondre <lukasondre@icloud.com>
2023-06-26 16:05:11 +0100

    Forward iframe console logs and uncaught errors

--- packages/networked-dom-web-runner/test/end-to-end.test.ts
+++ packages/networked-dom-web-runner/test/end-to-end.test.ts
@@ -3,6 +3,7 @@
  */
 
 import { EditableNetworkedDOM } from "@mml-io/networked-dom-document";
+import { LogMessage } from "@mml-io/observable-dom-common";
 import { AudioContext } from "standardized-audio-context-mock";
 
 import { waitFor } from "./test-util";
@@ -12,10 +13,15 @@ beforeAll(() => {
   (window as any).AudioContext = AudioContext;
 });
 test("networked-dom-web-runner end-to-end", async () => {
+  const logs: LogMessage[] = [];
+
   const networkedDOMDocument = new EditableNetworkedDOM(
     "http://example.com/index.html",
     IframeObservableDOMFactory,
     false,
+    (logMessage) => {
+      logs.push(logMessage);
+    },
   );
 
   const clientsHolder = document.createElement("div");
@@ -26,7 +32,17 @@ test("networked-dom-web-runner end-to-end", async () => {
   client.connect(networkedDOMDocument);
 
   networkedDOMDocument.load(
-    "<div data-some-id=\"test-element\" onclick=\"this.setAttribute('data-some-attr','new-value')\"></div>",
+    `<div 
+      data-some-id="test-element" 
+      onclick="
+        this.setAttribute('data-some-attr','new-value'); 
+        console.log('level-log'); 
+        console.info('level-info'); 
+        console.warn('level-warn'); 
+        console.error('level-error'); 
+        undef[1];
+      "
+    ></div>`,
   );
 
   await waitFor(() => {
@@ -36,6 +52,46 @@ test("networked-dom-web-runner end-to-end", async () => {
 
   testElement.dispatchEvent(new MouseEvent("click", { bubbles: true }));
   await waitFor(() => testElement.getAttribute("data-some-attr") === "new-value");
+
+  await waitFor(() => true);
+
+  expect(logs).toContainEqual(
+    expect.objectContaining({
+      level: "log",
+      content: ["level-log"],
+    }),
+  );
+
+  expect(logs).toContainEqual(
+    expect.objectContaining({
+      level: "info",
+      content: ["level-info"],
+    }),
+  );
+
+  expect(logs).toContainEqual(
+    expect.objectContaining({
+      level: "warn",
+      content: ["level-warn"],
+    }),
+  );
+
+  expect(logs).toContainEqual(
+    expect.objectContaining({
+      level: "error",
+      content: ["level-error"],
+    }),
+  );
+
+  expect(logs).toContainEqual(
+    expect.objectContaining({
+      level: "system",
+      content: expect.objectContaining({
+        message: "undef is not defined",
+        type: "ReferenceError",
+      }),
+    }),
+  );
 
   networkedDOMDocument.dispose();
 });
