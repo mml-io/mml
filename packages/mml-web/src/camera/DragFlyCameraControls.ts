b9020ca552b46e9ad9cc30e8dc09cc9c4f53e214
Soo Ling Lim <soolinglim@improbable.io>
2023-06-26 17:35:54 +0100

    Standalone client mobile support
    * Added support for zoom (pinch in and out)
    * Added support for pan (drag left and right)

--- packages/mml-web/src/camera/DragFlyCameraControls.ts
+++ packages/mml-web/src/camera/DragFlyCameraControls.ts
@@ -4,6 +4,26 @@ import { EventHandlerCollection } from "../utils/events/EventHandlerCollection";
 
 const WorldUp = new Vector3(0, 1, 0);
 
+// Initial touch points
+let initialDistance = 0;
+let currentDistance = 0;
+
+enum SwipeDirection {
+  Left = 'left',
+  Right = 'right',
+  Up = 'up',
+  Down = 'down',
+}
+
+// Variables to track touch coordinates and time
+let startX: number;
+let startY: number;
+let startTime: number;
+
+// Zoom parameters
+let zoomTimestamp;
+const debounceTime = 20;
+
 // Creates a set of 5DOF flight controls that requires dragging the mouse to move the rotation and position of the camera
 export class DragFlyCameraControls {
   private enabled = false;
@@ -48,6 +68,11 @@ export class DragFlyCameraControls {
     if (this.enabled) {
       return;
     }
+
+    document.addEventListener("touchstart", function(e){
+      e.preventDefault();
+    },{passive: false});
+
     this.enabled = true;
     this.eventHandlerCollection.add(document, "keydown", this.onKeyDown.bind(this));
     this.eventHandlerCollection.add(document, "keyup", this.onKeyUp.bind(this));
@@ -56,6 +81,9 @@ export class DragFlyCameraControls {
     this.eventHandlerCollection.add(this.domElement, "mousedown", this.onMouseDown.bind(this));
     this.eventHandlerCollection.add(document, "mouseup", this.onMouseUp.bind(this));
     this.eventHandlerCollection.add(document, "wheel", this.onMouseWheel.bind(this));
+    this.eventHandlerCollection.add(document, "touchstart", this.handleTouchStart.bind(this));
+    this.eventHandlerCollection.add(document, "touchend", this.handleTouchEnd.bind(this));
+    this.eventHandlerCollection.add(document, "touchmove", this.handleTouchMove.bind(this));
   }
 
   public disable() {
@@ -205,4 +233,105 @@ export class DragFlyCameraControls {
     // restrict to a reasonable min and max
     this.speed = Math.max(5, Math.min(this.speed, 1000));
   }
+
+  // Function to handle touch start event
+  private handleTouchStart(event: TouchEvent) {
+    const touch = event.touches[0];
+    startX = touch.clientX;
+    startY = touch.clientY;
+    startTime = Date.now();
+
+    const touchPoints = event.touches;
+    if (touchPoints.length === 2) {
+      // Calculate the initial distance between two touch points
+      const dx = touchPoints[0].clientX - touchPoints[1].clientX;
+      const dy = touchPoints[0].clientY - touchPoints[1].clientY;
+      initialDistance = Math.hypot(dx, dy);
+    }
+  }
+
+  // Function to handle touch end event
+  private handleTouchEnd(event: TouchEvent) {
+    event.preventDefault();
+    if (this.forward == true || this.backward == true){
+      zoomTimestamp = Date.now();
+    }
+    this.forward = false;
+    this.backward = false;
+    this.mouseDown = false;
+  }
+
+  // Function to handle touch move event
+  private handleTouchMove(event: TouchEvent) {
+    event.preventDefault();
+    const touch = event.touches[0];
+    const currentX = touch.clientX;
+    const currentY = touch.clientY;
+
+    const deltaX = currentX - startX;
+    const deltaY = currentY - startY;
+
+    // Calculate the distance and direction of the drag
+    const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
+    const angle = Math.atan2(deltaY, deltaX);
+    const angleInDegrees = angle * (180 / Math.PI);
+
+    const touchPoints = event.touches;
+    if (touchPoints.length === 2) {
+      // Zoom
+      // Calculate the current distance between two touch points
+      const dx = touchPoints[0].clientX - touchPoints[1].clientX;
+      const dy = touchPoints[0].clientY - touchPoints[1].clientY;
+      currentDistance = Math.hypot(dx, dy);
+
+      // Compare the change in distance to detect pinch gesture
+      const distanceDelta = currentDistance - initialDistance;
+      if (distanceDelta > 10) {
+        // Pinch out
+        this.forward = false;
+        this.backward = true;
+        this.mouseDown = true;
+      } else if (distanceDelta < -10) {
+        // Pinch in
+        this.forward = true;
+        this.backward = false;
+        this.mouseDown = true;
+      }
+    } else {
+      // Pan
+      if (!zoomTimestamp || Date.now() > zoomTimestamp + debounceTime) {
+        this.forward = false;
+        this.backward = false;
+        this.mouseDown = false;
+
+        // Update the start coordinates for the next move event
+        startX = currentX;
+        startY = currentY;
+
+        const movementX = deltaX;
+        let movementY = deltaY;
+
+        // This is an addition to the original PointerLockControls class
+        if (this.invertedMouseY) {
+          movementY *= -1;
+        }
+
+        this.tempEuler.setFromQuaternion(this.camera.quaternion);
+
+        this.tempEuler.y -= movementX * 0.002;
+        this.tempEuler.x -= movementY * 0.002;
+
+        this.tempEuler.x = Math.max(
+            Math.PI / 2 - this.maxPolarAngle,
+            Math.min(Math.PI / 2 - this.minPolarAngle, this.tempEuler.x),
+        );
+
+        this.camera.quaternion.setFromEuler(this.tempEuler);
+      }
+    }
+  }
+
+
+
+
 }
