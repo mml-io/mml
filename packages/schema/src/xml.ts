bf5fcf3c3f84c8ba8986933ad7bbf0e7c9f30050
Marcus Longmuir <marcus@mpl.dev>
2023-07-17 11:28:40 +0100

    Build TypeScript declarations (#36) (#75)

--- packages/schema/src/xml.ts
+++ packages/schema/src/xml.ts
@@ -1,7 +1,3 @@
 import schemaContents from "./schema-src/mml.xsd";
-// @ts-ignore
-// eslint-disable-next-line import/default
-import schemaEventsContents from "./schema-src/events.d.ts";
 
 export const schemaXML = schemaContents;
-export const eventsSchemaTS = schemaEventsContents;
