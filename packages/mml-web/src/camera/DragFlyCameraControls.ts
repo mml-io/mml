c95b004185a2da9dc3c9131063abf5147aea8627
Soo Ling Lim <soolinglim@improbable.io>
2023-08-01 13:35:14 +0100

    Update DragFlyCameraControls.ts

--- packages/mml-web/src/camera/DragFlyCameraControls.ts
+++ packages/mml-web/src/camera/DragFlyCameraControls.ts
@@ -345,7 +345,10 @@ export class DragFlyCameraControls {
       this.isMoving = true;
     } else if (event.touches.length === 1) {
       // Pan
-      if (!this.zoomTimestamp || Date.now() > this.zoomTimestamp + this.debounceThresholdMilliseconds) {
+      if (
+        !this.zoomTimestamp ||
+        Date.now() > this.zoomTimestamp + this.debounceThresholdMilliseconds
+      ) {
         this.isMoving = false;
 
         const movementX = event.touches[0].clientX - this.panStartX;
