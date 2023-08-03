93e7f5a9678ed8320f23d1436050e66bd576dc77
Sacha Morgese <sachamorgese@gmail.com>
2023-08-03 19:36:59 +0100

    Add logic to build react declaration file

--- packages/schema/src/schemaDefinition.test.ts
+++ packages/schema/src/schemaDefinition.test.ts
@@ -1,7 +1,10 @@
+import { createTSDefinitionFile, JSONSchema } from "./buildDeclarationFile";
 import { createSchemaDefinition, schemaJSON } from "../build/index";
 
 test("JSON schema created cubes", () => {
   const schemaDefinition = createSchemaDefinition(schemaJSON);
+
+  createTSDefinitionFile(schemaDefinition as JSONSchema);
 
   expect(schemaDefinition.elements["m-cube"].name).toEqual("m-cube");
 });
