2acf4a956b9cd7635222354278b0a9096ce26f30
Sacha Morgese <sachamorgese@gmail.com>
2023-08-09 13:59:29 +0100

    Created package for react types Created script to generate react types from XSD

---
+++ packages/mml-react-types/src/index.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+import * as url from "url";
+
+import { createSchemaDefinition, schemaJSON } from "@mml-io/mml-schema";
+
+import { createTSDefinitionFile, JSONSchema } from "./buildDeclarationFile.ts";
+
+const schemaDefinition = createSchemaDefinition(schemaJSON);
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const eventsFile = fs.readFileSync(
+  path.join(dirname, "../../schema/src/schema-src/events.d.ts"),
+  "utf-8",
+);
+
+const definitionFile = createTSDefinitionFile(schemaDefinition as JSONSchema, eventsFile);
+fs.mkdirSync(path.join(dirname, "../build"), { recursive: true });
+fs.writeFileSync(path.join(dirname, "../build/index.d.ts"), definitionFile);
--- packages/schema/src/schemaDefinition.test.ts
+++ packages/schema/src/schemaDefinition.test.ts
@@ -1,10 +1,7 @@
-import { createTSDefinitionFile, JSONSchema } from "./buildDeclarationFile";
 import { createSchemaDefinition, schemaJSON } from "../build/index";
 
 test("JSON schema created cubes", () => {
   const schemaDefinition = createSchemaDefinition(schemaJSON);
-
-  createTSDefinitionFile(schemaDefinition as JSONSchema);
 
   expect(schemaDefinition.elements["m-cube"].name).toEqual("m-cube");
 });
