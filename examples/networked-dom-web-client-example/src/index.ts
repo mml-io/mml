c6b7ce5a95d905ee2c25e25904c9326e56a814af
Sacha Morgese <sachamorgese@gmail.com>
2023-10-11 17:05:09 +0100

    test

--- examples/networked-dom-web-client-example/src/index.ts
+++ examples/networked-dom-web-client-example/src/index.ts
@@ -60,9 +60,7 @@ app.ws("/networked-dom-websocket", (ws: ws.WebSocket) => {
 });
 app.get("/websocket-example/", (req, res) => {
   res.send(
-    `<html><script src="http://192.168.50.117:28892/index.js?websocketUrl=${getWebsocketUrl(
-      req,
-    )}"></script></html>`,
+    `<html><script src="/client/index.js?websocketUrl=${getWebsocketUrl(req)}"></script></html>`,
   );
 });
 
