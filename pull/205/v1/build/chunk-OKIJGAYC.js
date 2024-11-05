b0c272217c0d58d3bd6e61bac6d8611573897401
MarcusLongmuir <MarcusLongmuir@users.noreply.github.com>
2024-11-05 13:30:26 +0000

    deploy: ac02146cfee788bfedd5de2cbf70c0a704e540c2

---
+++ pull/205/v1/build/chunk-OKIJGAYC.js
@@ -0,0 +1,15 @@
+// src/env-maps/puresky_2k.jpg
+var puresky_2k_default = "./puresky_2k-WS2HEI54.jpg";
+
+// src/env-maps/index.ts
+var envMaps = {
+  puresky: {
+    name: "Pure Sky",
+    url: puresky_2k_default
+  }
+};
+
+export {
+  envMaps
+};
+//# sourceMappingURL=chunk-OKIJGAYC.js.map
