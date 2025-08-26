cd82feabcefd38dbbcb31a64ccbe8fd4a371aae8
MarcusLongmuir <MarcusLongmuir@users.noreply.github.com>
2025-08-26 12:22:23 +0000

    deploy: f83b2befe77d5fad2c72f55eb99880076d3d5388

--- main/v1/index.js
+++ main/v1/index.js
@@ -15,7 +15,7 @@ import {
   rendererField,
   setDebugGlobals,
   urlField
-} from "./chunk-DMCTG2AN.js";
+} from "./chunk-EPLE3BMH.js";
 
 // src/ui/setUrlParam.ts
 function setUrlParam(name, value) {
@@ -306,7 +306,7 @@ var PlayCanvasMode = class {
   }
   async init() {
     this.internalMode = await (async () => {
-      const { PlayCanvasModeInternal } = await import("./PlayCanvasModeInternal-VDTUTAJL.js");
+      const { PlayCanvasModeInternal } = await import("./PlayCanvasModeInternal-VEOVQGCF.js");
       return new PlayCanvasModeInternal(
         this.windowTarget,
         this.targetForWrappers,
@@ -479,7 +479,7 @@ var ThreeJSMode = class {
   }
   async init() {
     this.internalMode = await (async () => {
-      const { ThreeJSModeInternal } = await import("./ThreeJSModeInternal-T7ANT6YB.js");
+      const { ThreeJSModeInternal } = await import("./ThreeJSModeInternal-ZRE65KB7.js");
       return new ThreeJSModeInternal(
         this.windowTarget,
         this.targetForWrappers,
